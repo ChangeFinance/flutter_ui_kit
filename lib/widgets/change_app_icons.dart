b0611ea295a0ad587e1e2c93a200665b01ba422c
olabodeadedoyin <olabode@getchange.com>
2019-05-05 23:51:23 +0300

    Added remove stadalon mode from static files

--- lib/widgets/change_app_icons.dart
+++ lib/widgets/change_app_icons.dart
@@ -9,40 +9,35 @@ class ChangeAppIcons {
       const ChangeIconData(
           'atm',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon atm_withdraw_disabled = ChangeIcon(
       const ChangeIconData(
           'atm',
           ChangeIconType.disabled
-      ),
-    isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon buy = ChangeIcon(
       const ChangeIconData(
           'buy',
           ChangeIconType.enabled
-      ),
-    isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon buy_disabled = ChangeIcon(
       const ChangeIconData(
           'buy',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon card_payment = ChangeIcon(
       const ChangeIconData(
           'card_payment',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon card_payment_disabled = ChangeIcon(
@@ -50,134 +45,118 @@ class ChangeAppIcons {
           'card_payment',
           ChangeIconType.disabled
       ),
-      isStandAloneMode: true
+
   );
 
   static const ChangeIcon convert = ChangeIcon(
       const ChangeIconData(
           'convert',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon convert_disabled = ChangeIcon(
       const ChangeIconData(
           'convert',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon fees = ChangeIcon(
       const ChangeIconData(
           'fees',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon fees_disabled = ChangeIcon(
       const ChangeIconData(
           'fees',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon deposit = ChangeIcon(
       const ChangeIconData(
           'deposit',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon deposit_disabled = ChangeIcon(
       const ChangeIconData(
           'deposit',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon sell = ChangeIcon(
       const ChangeIconData(
           'sell',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon sell_disabled = ChangeIcon(
       const ChangeIconData(
           'sell',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon withdraw = ChangeIcon(
       const ChangeIconData(
           'withdraw',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon withdraw_disabled = ChangeIcon(
       const ChangeIconData(
           'withdraw',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true,
+      )
   );
 
   static const ChangeIcon roundup = ChangeIcon(
       const ChangeIconData(
           'roundup',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon roundup_disabled = ChangeIcon(
       const ChangeIconData(
           'roundup',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon bitcoin_to_savings = ChangeIcon(
       const ChangeIconData(
           'bitcoin_to_savings',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon bitcoin_to_savings_disabled = ChangeIcon(
       const ChangeIconData(
           'bitcoin_to_savings',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon wallet = ChangeIcon(
       const ChangeIconData(
           'wallet',
           ChangeIconType.enabled
-      ),
-      isStandAloneMode: true
+      )
   );
 
   static const ChangeIcon wallet_disabled = ChangeIcon(
       const ChangeIconData(
           'wallet',
           ChangeIconType.disabled
-      ),
-      isStandAloneMode: true
+      )
   );
 }
