458bc20305a274d037244192f1319eae8cff8d92
Vinay Puranik <vinaypuranik564@gmail.com>
2019-05-29 10:52:08 +0300

    implemented customizable header and body style for text pair component

--- lib/widgets/text/text_pair.dart
+++ lib/widgets/text/text_pair.dart
@@ -4,20 +4,23 @@ import 'package:flutter_ui_kit/text.dart';
 class TextPair extends StatelessWidget {
   final String heading;
   final String body;
+  final TextStyle bodyStyle;
+  final TextStyle headingStyle;
 
-  const TextPair(this.heading, this.body);
+  const TextPair(this.heading, this.body, {this.headingStyle = AppText.header3,
+    this.bodyStyle = AppText.greyBody2});
 
   @override
   Widget build(BuildContext context) {
     return new Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(heading, style: AppText.header3, textAlign: TextAlign.left),
+        Text(heading, style: headingStyle, textAlign: TextAlign.left),
         Padding(
             padding: const EdgeInsets.only(top: 10.0),
             child: Text(
               body,
-              style: AppText.greyBody2,
+              style: bodyStyle,
             ))
       ],
     );
