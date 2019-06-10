dbb24cda9c4d9f27193f1dfefe87f098dd8fc300
VitalyKucher <vkucher@rightandabove.com>
2019-06-10 14:27:58 +0300

    add init properties

--- lib/widgets/sparkline.dart
+++ lib/widgets/sparkline.dart
@@ -1,13 +1,11 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 import 'package:flutter_sparkline/flutter_sparkline.dart';
 
-math.Random random = new math.Random();
-
 class SparkLine extends StatefulWidget {
 
-  const SparkLine({Key key}) : super(key: key);
+  final bool enableMaxMin;
+  final String labelPrefix;
+  SparkLine({Key key, this.enableMaxMin = false, this.labelPrefix = '€'}) : super(key: key);
 
   @override
   SparkLineWidgetState createState() => SparkLineWidgetState();
@@ -15,40 +13,34 @@ class SparkLine extends StatefulWidget {
 
 class SparkLineWidgetState extends State<SparkLine> {
 
-  static List<double> _generateRandomData(int count) {
-    final result = <double>[];
-    for (var i = 0; i < count; i++) {
-      result.add(random.nextDouble() * 10000);
-    }
-    return result;
-  }
+  List<double> _data = [0.0];
 
-  List<double> _data = _generateRandomData(50);
-
-  void onRefresh() {
+  void onRefresh(List<double> data) {
     setState(() {
-      _data = _generateRandomData(50);
+      _data = data;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Center(
-        child: new Container(
-            child: new Sparkline(
-      data: _data,
-      lineColor: const Color.fromRGBO(62, 219, 181, 1),
-      lineWidth: 1.0,
-      pointsMode: PointsMode.none,
-      fillMode: FillMode.below,
-      fillGradient: new LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: const [
-          Color.fromRGBO(114, 239, 219, 0.13),
-          Color.fromRGBO(62, 219, 181, 0)
-        ],
-      ),
+      child: new Container(
+        child: new Sparkline(
+        data: _data,
+        lineColor: const Color.fromRGBO(62, 219, 181, 1),
+        lineWidth: 1.0,
+        pointsMode: PointsMode.none,
+        fillMode: FillMode.below,
+        enableMaxMin: widget.enableMaxMin,
+        labelPrefix: widget.labelPrefix,
+        fillGradient: new LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: const [
+            Color.fromRGBO(114, 239, 219, 0.13),
+            Color.fromRGBO(62, 219, 181, 0)
+          ],
+        ),
     )));
   }
 }
