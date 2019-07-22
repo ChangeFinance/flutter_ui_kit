0090d498bef3c52b84c30924d2cf956c63dc89bf
Vinay Puranik <vinaypuranik564@gmail.com>
2019-07-22 16:00:04 +0300

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_ui_kit/stories/buttons.dart';
 import 'package:flutter_ui_kit/stories/cards.dart';
 import 'package:flutter_ui_kit/stories/colors.dart';
 import 'package:flutter_ui_kit/stories/layouts.dart';
 import 'package:flutter_ui_kit/stories/numpads.dart';
 import 'package:flutter_ui_kit/stories/popover.dart';
-import 'package:flutter_ui_kit/stories/graphs.dart';
 import 'package:flutter_ui_kit/stories/sliders.dart';
 import 'package:flutter_ui_kit/stories/typography.dart';
 import 'package:flutter_ui_kit/stories/icons.dart';
