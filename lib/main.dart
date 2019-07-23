c26452c1e39091ccafc074b1b2cce0ef17051fe4
SZdobnikova <szdobnikova@rightandabove.com>
2019-07-23 10:20:07 +0300

    implementation of custom date picker

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:flutter_ui_kit/stories/wizard.dart';
 import 'package:flutter_ui_kit/story_book/story_book.dart';
 import 'package:flutter_ui_kit/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_kit/widgets/custom_date_picker.dart';
 import 'package:flutter_ui_kit/widgets/customradio/custom_radio.dart';
 import 'package:flutter_ui_kit/widgets/layout/sliver_template.dart';
 import 'package:flutter_ui_kit/widgets/sliver_widgets.dart';
@@ -43,7 +44,8 @@ void main() {
           Sliders(),
           WizardStory(),
           Sliders(),
-          CustomRadio()
+          CustomRadio(),
+          CustomDatePicker()
         ],
       ),
     ),
