8db2c38bb8885a1e7809fe061d83519f3611b19c
SZdobnikova <szdobnikova@rightandabove.com>
2019-08-27 16:51:13 +0300

    fix for  failed build

--- lib/stories/custom_label_value.dart
+++ lib/stories/custom_label_value.dart
@@ -1,13 +1,5 @@
-import 'package:flutter_ui_kit/story_book/expandable_story.dart';
-import 'package:flutter_ui_kit/story_book/prop_updater/bool_prop_updater.dart';
-import 'package:flutter_ui_kit/story_book/prop_updater/int_prop_updater.dart';
-import 'package:flutter_ui_kit/story_book/prop_updater/string_prop_updater.dart';
-import 'package:flutter_ui_kit/story_book/props_explorer.dart';
 import 'package:flutter_ui_kit/widgets.dart';
-import 'package:flutter_ui_kit/widgets/filled_button.dart';
-import 'package:flutter_ui_kit/widgets/outlined_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_kit/widgets/text_button.dart';
 
 class CustomLabelValue extends StatelessWidget {
   @override
