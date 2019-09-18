8ec2090e70543e2e45a30f4a3a7c9b6385e82b79
Vadim Gulyakin <vgulyakin@rightandabove.com>
2019-09-18 16:01:50 +0300

    merged from master

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,16 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-#import <camera/CameraPlugin.h>
 #import <package_info/PackageInfoPlugin.h>
-#import <path_provider/PathProviderPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
   [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
-  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
 }
 
 @end
