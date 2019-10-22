fe45886234f819c95372bff41e3ac0f6a193123e
valeriya makusheva <vmakusheva@rightandabove.com>
2019-10-22 09:12:17 +0300

    update two states button to reset its state on build if necessary

--- lib/widgets/two_states_button.dart
+++ lib/widgets/two_states_button.dart
@@ -15,6 +15,7 @@ class TwoStatesButton extends StatefulWidget {
   final bool narrow;
   final EdgeInsetsGeometry padding;
   final TextStyle textStyle;
+  final bool resetButtonOnBuild;
 
   TwoStatesButton(
       this.initialText,
@@ -26,6 +27,7 @@ class TwoStatesButton extends StatefulWidget {
         this.narrow = false,
         this.padding,
         this.textStyle,
+        this.resetButtonOnBuild = false,
         Key key,
       })  : assert(initialText != null),
         super(key: key);
@@ -38,16 +40,19 @@ class _TwoStatesButtonState extends State<TwoStatesButton> with ButtonMixin {
   bool _enabled = true;
   Timer _timer;
   String _currentText;
+  bool _resetState = false;
 
   @override
   void initState() {
     super.initState();
+    _resetState = widget.resetButtonOnBuild;
     _startTimer();
   }
 
   void _startTimer() {
     setState(() => _currentText = widget.initialText);
     _timer = new Timer(Duration(seconds: widget.timeIntervalInSec), () {
+      _resetState = false;
       setState(() => _currentText = widget.finalText);
     });
   }
@@ -58,8 +63,20 @@ class _TwoStatesButtonState extends State<TwoStatesButton> with ButtonMixin {
     super.dispose();
   }
 
+  void _resetTimer() {
+    if (_resetState) {
+      if (_timer.isActive) {
+        _timer.cancel();
+      }
+      _startTimer();
+    } else {
+      _resetState = widget.resetButtonOnBuild;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    _resetTimer();
     return Container(
       width: widget.fullWidth ? matchParentWidth(context) : null,
       child: RaisedButton(
