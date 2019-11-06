47912a78c3ed6a385af6aeac8c238e7f68300302
Tomi Alagbe <tomialagbe@yahoo.com>
2019-11-06 17:21:32 +0200

    modify passcode numpad

--- lib/widgets/text/passcode_numpad_text.dart
+++ lib/widgets/text/passcode_numpad_text.dart
@@ -6,22 +6,14 @@ typedef Callback = void Function(String value);
 
 class PasscodeNumPadText extends StatefulWidget {
   final Callback onChange;
-  final bool clearOnLongPress;
   final int textLengthLimit;
-  final String startNumPadText;
-  final bool needNumPadTextUpdate;
-  final bool noTextCache;
   final Callback onKey;
   final String actionButtonText;
   final VoidCallback onActionbuttonPressed;
 
   const PasscodeNumPadText({
     @required this.onChange,
-    this.clearOnLongPress = false,
     this.textLengthLimit = 0,
-    this.startNumPadText = '',
-    this.needNumPadTextUpdate = false,
-    this.noTextCache = false,
     this.onKey,
     this.actionButtonText,
     this.onActionbuttonPressed,
@@ -34,77 +26,39 @@ class PasscodeNumPadText extends StatefulWidget {
 class _PasscodeNumPadTextState extends State<PasscodeNumPadText> {
   String _text = '';
 
-  bool _checkPressedClear(String key) {
-    if (key != 'C') {
-      return false;
-    }
-
-    if (_text.length > 1) {
-      _text = _text.substring(0, _text.length - 1);
-    } else {
-      _text = '0';
-    }
-
-    return true;
-  }
-
-  bool _checkPressedKey(String key) {
-    if (!'0123456789'.contains(key)) {
-      return false;
-    }
-
-    if (_text == '0') {
-      if (key == '0') {
-        _text = '0.';
-      } else if (key != '.') {
-        _text = '';
-      }
-    }
-
-    if (widget.textLengthLimit > 0 && (_text + key).length > widget.textLengthLimit) {
-      return false;
-    }
-    _text += key;
-    return true;
-  }
-
   void onKeyTapped(String key) {
-    if (widget.noTextCache && widget.onKey != null) {
-      widget.onKey(key);
+    if (!'0123456789C'.contains(key)) {
       return;
     }
 
-    if (widget.needNumPadTextUpdate) {
-      _text = widget.startNumPadText;
+    final isCancel = 'C' == key;
+
+    if (widget.onKey != null) {
+      widget.onKey(key);
     }
 
-    if (!_checkPressedClear(key)) {
-      if (!_checkPressedKey(key)) {
+    if (!isCancel) {
+      if (widget.textLengthLimit > 0 && (_text + key).length > widget.textLengthLimit) {
         return;
       }
+      _text += key;
+    } else {
+      _text = _text.isNotEmpty ? _text.substring(0, _text.length - 1) : '';
     }
 
     widget.onChange(_text);
-  }
-
-  void onKeyLongPressed(String key) {
-    if (key == 'C' && widget.clearOnLongPress) {
-      _text = '';
-      widget.onChange(_text);
-    }
   }
 
   Widget buildKeyItem(String val) {
     final isActionButton = val == widget.actionButtonText;
     final actionButtonStyle =
         AppText.numPadTextStyle.copyWith(color: AppColor.darkerGreen, fontWeight: FontWeight.normal, fontSize: 16);
-    return KeyItem(
+    return _KeyItem(
       value: val,
       child: (val != 'C')
           ? Text(val, textAlign: TextAlign.center, style: isActionButton ? actionButtonStyle : AppText.numPadTextStyle)
           : const Icon(Icons.arrow_back, size: 24.0, color: AppColor.deepBlack),
       onKeyTap: onKeyTapped,
-      onKeyLongPress: onKeyLongPressed,
     );
   }
 
@@ -126,13 +80,12 @@ class _PasscodeNumPadTextState extends State<PasscodeNumPadText> {
   }
 }
 
-class KeyItem extends StatelessWidget {
+class _KeyItem extends StatelessWidget {
   final Widget child;
   final String value;
   final Function(String value) onKeyTap;
-  final Function(String value) onKeyLongPress;
 
-  const KeyItem({@required this.child, this.value, this.onKeyTap, this.onKeyLongPress});
+  const _KeyItem({@required this.child, this.value, this.onKeyTap});
 
   @override
   Widget build(BuildContext context) {
@@ -141,7 +94,6 @@ class KeyItem extends StatelessWidget {
             radius: 30,
             splashColor: AppColor.brightGreen,
             highlightColor: Colors.white,
-            onLongPress: () => onKeyLongPress(value),
             onTap: () => onKeyTap(value),
             child: Container(alignment: Alignment.center, child: child)));
   }
