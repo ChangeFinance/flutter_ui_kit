ae12ce6f64b127ba7eb0831d506e4217ff10e5b9
Almas Akchabayev <almeynman@gmail.com>
2019-11-13 23:14:52 +0200

    add shadow

--- lib/widgets/layout/sliver_template.dart
+++ lib/widgets/layout/sliver_template.dart
@@ -4,7 +4,9 @@ import 'package:flutter_ui_kit/color.dart';
 
 import 'custom_flexible_space_bar.dart';
 
-class SliverTemplate extends StatelessWidget {
+class SliverTemplate extends StatefulWidget {
+  static double heightAsPercentageOfScreenWidth = 0.7; 
+
   final Widget pinWidget;
   final Widget content;
   final List<Widget> sliverList;
@@ -13,7 +15,7 @@ class SliverTemplate extends StatelessWidget {
   final Widget backgroundWidget;
   final FloatingActionButton floatingActionButton;
   final ScrollController scrollController;
-  final double heightAsPercentageOfScreenWidth;
+
 
   const SliverTemplate({
     this.content = const SizedBox.shrink(),
@@ -24,57 +26,94 @@ class SliverTemplate extends StatelessWidget {
     this.backgroundWidget = const SizedBox.shrink(),
     this.floatingActionButton,
     this.scrollController,
-    this.heightAsPercentageOfScreenWidth = 0.7,
   });
 
-  double _getSliverAppBarHeight(BuildContext context) {
+  static double getSliverAppBarHeight(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     return screenWidth * heightAsPercentageOfScreenWidth;
   }
 
   @override
+  _SliverTemplateState createState() => _SliverTemplateState();
+}
+
+class _SliverTemplateState extends State<SliverTemplate> {
+  final GlobalKey topAppBarKey = GlobalKey<_SliverTemplateState>();
+
+  bool collapsed = false;
+
+  void _scrollListener(double offset, BuildContext context) {
+    final sliverAppBarHeight = SliverTemplate.getSliverAppBarHeight(context);
+    final topAppBarBox =
+        // ignore: avoid_as
+        topAppBarKey.currentContext.findRenderObject() as RenderBox;
+    final topAppBarHeight = topAppBarBox.size.height;
+    if (sliverAppBarHeight - offset <= topAppBarHeight) {
+      setState(() {
+        collapsed = true;
+      });
+    } else {
+      setState(() {
+        collapsed = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      floatingActionButton: floatingActionButton,
-      body: CustomScrollView(
-        controller: scrollController,
-        physics: const BouncingScrollPhysics(),
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: _getSliverAppBarHeight(context),
-            pinned: isPinned,
-            forceElevated: true,
-            elevation: 2,
-            automaticallyImplyLeading: false,
-            backgroundColor: AppColor.deepWhite,
-            title: appBarTitleWidget,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 20.0,
+      floatingActionButton: widget.floatingActionButton,
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (sn) {
+          _scrollListener(sn.metrics.pixels, context);
+        },
+        child: CustomScrollView(
+          controller: widget.scrollController,
+          physics: const BouncingScrollPhysics(),
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: SliverTemplate.getSliverAppBarHeight(context),
+              pinned: widget.isPinned,
+              forceElevated: collapsed,
+              elevation: collapsed ? 1 : 0,
+              automaticallyImplyLeading: false,
+              backgroundColor: AppColor.deepWhite,
+              title: Container(
+                key: topAppBarKey,
+                constraints: const BoxConstraints.expand(),
+                child: widget.appBarTitleWidget,
+              ),
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  boxShadow: collapsed
+                      ? []
+                      : [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 20.0,
+                          ),
+                        ],
                 ),
-              ]),
-              child: CustomFlexibleSpaceBar(
-                centerTitle: true,
-                title: content,
-                collapseMode: CollapseMode.pin,
-                background: Container(
-                  child: backgroundWidget,
-                  decoration: const BoxDecoration(
-                    color: AppColor.deepWhite,
+                child: CustomFlexibleSpaceBar(
+                  centerTitle: true,
+                  title: widget.content,
+                  collapseMode: CollapseMode.pin,
+                  background: Container(
+                    child: widget.backgroundWidget,
+                    decoration: const BoxDecoration(
+                      color: AppColor.deepWhite,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              sliverList,
+            SliverList(
+              delegate: SliverChildListDelegate(
+                widget.sliverList,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
