fae3e0f1c795b5a3745d0a5b855a54e851868791
Almas Akchabayev <almas.akchabayev@gmail.com>
2019-11-14 10:12:10 +0200

    Add elevation to sliver app bar (#131)
    * add elevation to sliver app bar
    
    * remove changing opacity on resize
    
    * refactor
    
    * add shadow
    
    * resolve lint error

--- lib/widgets/layout/sliver_template.dart
+++ lib/widgets/layout/sliver_template.dart
@@ -4,7 +4,9 @@ import 'package:flutter_ui_kit/color.dart';
 
 import 'custom_flexible_space_bar.dart';
 
-class SliverTemplate extends StatelessWidget {
+class SliverTemplate extends StatefulWidget {
+  static double heightAsPercentageOfScreenWidth = 0.7; 
+
   final Widget pinWidget;
   final Widget content;
   final List<Widget> sliverList;
@@ -13,48 +15,106 @@ class SliverTemplate extends StatelessWidget {
   final Widget backgroundWidget;
   final FloatingActionButton floatingActionButton;
   final ScrollController scrollController;
-  static const appBarWidthPercentage = 0.7;
 
-  const SliverTemplate(
-      {this.content = const SizedBox.shrink(),
-      this.sliverList,
-      this.pinWidget = const SizedBox.shrink(),
-      this.isPinned = true,
-      this.appBarTitleWidget = const SizedBox.shrink(),
-      this.backgroundWidget = const SizedBox.shrink(),
-      this.floatingActionButton,
-      this.scrollController});
+
+  const SliverTemplate({
+    this.content = const SizedBox.shrink(),
+    this.sliverList,
+    this.pinWidget = const SizedBox.shrink(),
+    this.isPinned = true,
+    this.appBarTitleWidget = const SizedBox.shrink(),
+    this.backgroundWidget = const SizedBox.shrink(),
+    this.floatingActionButton,
+    this.scrollController,
+  });
+
+  static double getSliverAppBarHeight(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return screenWidth * heightAsPercentageOfScreenWidth;
+  }
+
+  @override
+  _SliverTemplateState createState() => _SliverTemplateState();
+}
+
+class _SliverTemplateState extends State<SliverTemplate> {
+  final GlobalKey topAppBarKey = GlobalKey<_SliverTemplateState>();
+
+  bool collapsed = false;
+
+  void _scrollListener(double offset, BuildContext context) {
+    final sliverAppBarHeight = SliverTemplate.getSliverAppBarHeight(context);
+    final topAppBarBox =
+        // ignore: avoid_as
+        topAppBarKey.currentContext.findRenderObject() as RenderBox;
+    final topAppBarHeight = topAppBarBox.size.height;
+    if (sliverAppBarHeight - offset <= topAppBarHeight) {
+      setState(() {
+        collapsed = true;
+      });
+    } else {
+      setState(() {
+        collapsed = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-        floatingActionButton: floatingActionButton,
-        body: CustomScrollView(
-            controller: scrollController,
-            physics: const BouncingScrollPhysics(),
-            slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: mediaQuery.size.width * appBarWidthPercentage,
-            pinned: isPinned,
-            forceElevated: false,
-            elevation: 0,
-            automaticallyImplyLeading: false,
-            backgroundColor: AppColor.deepWhite,
-            title: appBarTitleWidget,
-            flexibleSpace: CustomFlexibleSpaceBar(
-              centerTitle: true,
-              title: content,
-              collapseMode: CollapseMode.pin,
-              background: Container(
-                  child: backgroundWidget,
-                  decoration: const BoxDecoration(color: AppColor.deepWhite)),
+      floatingActionButton: widget.floatingActionButton,
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (sn) {
+          _scrollListener(sn.metrics.pixels, context);
+        },
+        child: CustomScrollView(
+          controller: widget.scrollController,
+          physics: const BouncingScrollPhysics(),
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: SliverTemplate.getSliverAppBarHeight(context),
+              pinned: widget.isPinned,
+              forceElevated: collapsed,
+              elevation: collapsed ? 1 : 0,
+              automaticallyImplyLeading: false,
+              backgroundColor: AppColor.deepWhite,
+              title: Container(
+                key: topAppBarKey,
+                constraints: const BoxConstraints.expand(),
+                child: widget.appBarTitleWidget,
+              ),
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  boxShadow: collapsed
+                      ? []
+                      : [
+                          const BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 20.0,
+                          ),
+                        ],
+                ),
+                child: CustomFlexibleSpaceBar(
+                  centerTitle: true,
+                  title: widget.content,
+                  collapseMode: CollapseMode.pin,
+                  background: Container(
+                    child: widget.backgroundWidget,
+                    decoration: const BoxDecoration(
+                      color: AppColor.deepWhite,
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
-          SliverList(
+            SliverList(
               delegate: SliverChildListDelegate(
-            sliverList,
-          )),
-        ]));
+                widget.sliverList,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
