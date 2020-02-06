a6fb2745c601abf50873abb31c6fb75070d7b858
mustafatufan <mustafa@tufan.ee>
2020-02-06 16:56:03 +0200

    - added error to story

--- lib/stories/inputs.dart
+++ lib/stories/inputs.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_kit/story_book/expandable_story.dart';
+import 'package:flutter_ui_kit/story_book/prop_updater/bool_prop_updater.dart';
+import 'package:flutter_ui_kit/story_book/prop_updater/string_prop_updater.dart';
 import 'package:flutter_ui_kit/story_book/props_explorer.dart';
 import 'package:flutter_ui_kit/widgets/text/email.dart';
 
@@ -18,16 +20,27 @@ class Inputs extends StatelessWidget {
     return ExpandableStory(
       title: 'Email',
       child: PropsExplorer(
-        initialProps: const <String, dynamic>{},
+        initialProps: const <String, dynamic>{'error': false, 'errorText': ''},
         formBuilder: (context, props, updateProp) {
           return ListView(
             physics: const NeverScrollableScrollPhysics(),
             shrinkWrap: true,
-            children: const <Widget>[],
+            children: <Widget>[
+              BoolPropUpdater(
+                props: props,
+                updateProp: updateProp,
+                propKey: 'error',
+              ),
+              StringPropUpdater(
+                props: props,
+                updateProp: updateProp,
+                propKey: 'errorText',
+              )
+            ],
           );
         },
         widgetBuilder: (context, props) {
-          return const Email();
+          return Email(errorText: props['error'] ? props['errorText'] : null);
         },
       ),
     );
