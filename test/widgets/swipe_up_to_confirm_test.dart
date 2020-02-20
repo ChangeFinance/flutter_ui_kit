2a05d6feb4f7cfdea004c0b575297bcb396c3f45
Tomi Alagbe <tomialagbe@yahoo.com>
2020-02-20 12:53:47 +0200

    remove unused code

--- test/widgets/swipe_up_to_confirm_test.dart
+++ test/widgets/swipe_up_to_confirm_test.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ui_kit/widgets/layout/swipe_up_to_confirm.dart';
-import 'package:mockito/mockito.dart';
 
 import '../wrap_in_material_app.dart';
-
-class MockFunction extends Mock implements Function {}
 
 void main() {
   group('swipe up to confirm view', () {
