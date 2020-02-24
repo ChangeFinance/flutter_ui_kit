41ad5b568f48d24c9ae14bf8c61f80a2b5ab61f5
Mustafa Tufan <mustafa@tufan.ee>
2020-02-24 19:05:39 +0200

    EN 739 (#148) added transaction list item container
    * - added transaction list item container
    * - added transaction list item container to demo

---
+++ lib/widgets/list_items/transaction_list_item_container.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../color.dart';
+
+class TransactionListItemContainer extends StatelessWidget {
+  final String title;
+  final List<Widget> transactionListItems;
+
+  const TransactionListItemContainer({this.title, this.transactionListItems});
+
+  @override
+  Widget build(BuildContext context) {
+    if (transactionListItems.isEmpty) {
+      return const SizedBox.shrink();
+    }
+    final widgets = <Widget>[];
+    widgets.add(_getTitle(title));
+    widgets.addAll(transactionListItems);
+
+    return Container(
+        decoration: BoxDecoration(
+            border: Border(
+          bottom: BorderSide(width: 1.0, color: AppColor.lightestGrey),
+        )),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, children: widgets));
+  }
+
+  Widget _getTitle(String _title) {
+    return Padding(
+        padding: const EdgeInsets.only(top: 24.0, bottom: 16.0),
+        child: Text(_title,
+            textAlign: TextAlign.left,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: _greyTextStyle()));
+  }
+
+  TextStyle _greyTextStyle() {
+    return const TextStyle(
+        height: 1.428,
+        fontSize: 14.0,
+        fontFamily: 'Circular',
+        fontWeight: FontWeight.normal,
+        letterSpacing: 0.02,
+        color: AppColor.semiGrey);
+  }
+}
