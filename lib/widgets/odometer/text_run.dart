088479f61e5575fc6629a761eb3d16d21e38dd6c
Tomi Alagbe <tomialagbe@yahoo.com>
2020-04-09 11:32:19 +0100

    Al 176 odometer widget (#168)
    * Update sparkline version
    
    * Implement odometer widget
    
    * Add curve to animation
    
    * Fix linter issues
    
    * fix linter issue

---
+++ lib/widgets/odometer/text_run.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TextRun {
+  final String initialText;
+  final String currentText;
+  final TextStyle textStyle;
+
+  TextRun(this.currentText, this.initialText, this.textStyle)
+      : assert(isNumberOrDot(currentText)),
+        assert(isNumberOrDot(initialText)),
+        assert(isValidPair(currentText, initialText));
+
+  static Size getSingleDigitSize(String digit, TextStyle textStyle) {
+    return TextRun(digit, '0', textStyle).getSize();
+  }
+
+  static bool isNumberOrDot(String text) {
+    return num.tryParse(text) != null || isDot(text);
+  }
+
+  static bool isDot(String text) => '.' == text;
+
+  static bool isValidPair(String current, String initial) {
+    if (isDot(initial) && !isDot(current)) {
+      return false;
+    }
+
+    if ((!isDot(current) && current.endsWith('.')) || (!isDot(initial) && initial.endsWith('.'))) {
+      return false;
+    }
+    return true;
+  }
+
+  Size getSize() {
+    final painter = TextPainter();
+    painter.text = TextSpan(style: textStyle, text: '$currentText');
+    painter.textDirection = TextDirection.ltr;
+    painter.textAlign = TextAlign.left;
+    painter.textScaleFactor = 1.0;
+    painter.layout();
+    return painter.size;
+  }
+}
