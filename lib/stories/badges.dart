b69e6c5532e9b1c2661a8c6cc0e21ab73926235e
Tomi Alagbe <tomialagbe@yahoo.com>
2020-04-14 04:36:28 +0100

    add odometer to badge

--- lib/stories/badges.dart
+++ lib/stories/badges.dart
@@ -6,6 +6,7 @@ import 'package:flutter_ui_kit/story_book/prop_updater/string_prop_updater.dart'
 import 'package:flutter_ui_kit/story_book/props_explorer.dart';
 import 'package:flutter_ui_kit/widgets/badge/badge.dart';
 import 'package:flutter_ui_kit/widgets/badge/multivalue_badge.dart';
+import 'package:flutter_ui_kit/widgets/badge/odometer_badge.dart';
 
 import '../color.dart';
 
@@ -24,8 +25,9 @@ class Badges extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: MediaQuery.of(context).size.width,
-        child: SingleChildScrollView(child: _badgesStory()));
+      width: MediaQuery.of(context).size.width,
+      child: SingleChildScrollView(child: _badgesStory()),
+    );
   }
 }
 
@@ -33,57 +35,45 @@ Widget _badgesStory() {
   return Container(
       child: SingleChildScrollView(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _simpleBadgeStory(),
-              _multiValueBadgeStory()
-            ]
-          )
-  ));
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      _simpleBadgeStory(),
+      _multiValueBadgeStory(),
+      _OdometerBadgeStory(),
+    ],
+  )));
 }
 
 Widget _simpleBadgeStory() {
   return ExpandableStory(
       title: 'Simple Badge',
       child: PropsExplorer(
-          initialProps: const <String, dynamic>{
-            'text': '\$1234.56',
-            'minWidth': 80.0,
-            'bgColor': 'Green'
-          },
+          initialProps: const <String, dynamic>{'text': '\$1234.56', 'minWidth': 80.0, 'bgColor': 'Green'},
           formBuilder: (context, props, updateProp) {
-            return ListView(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                children: <Widget>[
-                  StringPropUpdater(
-                    props: props,
-                    updateProp: updateProp,
-                    propKey: 'text',
-                  ),
-                  DoublePropUpdater(
-                      props: props,
-                      updateProp: updateProp,
-                      propKey: 'minWidth',
-                      min: 60.0,
-                      max: 160.0),
-                  DropdownPropUpdater(
-                    props: props,
-                    updateProp: updateProp,
-                    propKey: 'bgColor',
-                    options: _colorList,
-                  )
-                ]);
+            return ListView(physics: const NeverScrollableScrollPhysics(), shrinkWrap: true, children: <Widget>[
+              StringPropUpdater(
+                props: props,
+                updateProp: updateProp,
+                propKey: 'text',
+              ),
+              DoublePropUpdater(props: props, updateProp: updateProp, propKey: 'minWidth', min: 60.0, max: 160.0),
+              DropdownPropUpdater(
+                props: props,
+                updateProp: updateProp,
+                propKey: 'bgColor',
+                options: _colorList,
+              )
+            ]);
           },
           widgetBuilder: (context, props) {
             return Column(mainAxisSize: MainAxisSize.min, children: [
               Container(
                   child: Center(
                       child: Badge(
-                        props['text'],
-                        minWidth: props['minWidth'],
-                        bgColor: _optionToColor(props['bgColor']),
-                      ))),
+                props['text'],
+                minWidth: props['minWidth'],
+                bgColor: _optionToColor(props['bgColor']),
+              ))),
             ]);
           }));
 }
@@ -98,35 +88,105 @@ Widget _multiValueBadgeStory() {
             'bgColor': 'Green'
           },
           formBuilder: (context, props, updateProp) {
-            return ListView(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                children: <Widget>[
-                  DoublePropUpdater(
-                      props: props,
-                      updateProp: updateProp,
-                      propKey: 'minWidth',
-                      min: 60.0,
-                      max: 160.0),
-                  DropdownPropUpdater(
-                    props: props,
-                    updateProp: updateProp,
-                    propKey: 'bgColor',
-                    options: _colorList,
-                  )
-                ]);
+            return ListView(physics: const NeverScrollableScrollPhysics(), shrinkWrap: true, children: <Widget>[
+              DoublePropUpdater(props: props, updateProp: updateProp, propKey: 'minWidth', min: 60.0, max: 160.0),
+              DropdownPropUpdater(
+                props: props,
+                updateProp: updateProp,
+                propKey: 'bgColor',
+                options: _colorList,
+              )
+            ]);
           },
           widgetBuilder: (context, props) {
-            return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    child: MultiValueBadge(
-                        props['texts'],
-                        minWidth: props['minWidth'],
-                        bgColor: _optionToColor(props['bgColor']),
-                      )),
+            return Column(mainAxisSize: MainAxisSize.min, children: [
+              Container(
+                  child: MultiValueBadge(
+                props['texts'],
+                minWidth: props['minWidth'],
+                bgColor: _optionToColor(props['bgColor']),
+              )),
             ]);
           }));
 }
 
+// ignore: must_be_immutable
+class _OdometerBadgeStory extends StatelessWidget {
+  double _initial = 0.00;
+  double _current = 0.00;
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = const TextStyle(color: AppColor.deepWhite, fontSize: 15);
+    return ExpandableStory(
+      title: 'Odometer Badge',
+      child: PropsExplorer(
+        initialProps: const <String, dynamic>{'bgColor': 'Green'},
+        formBuilder: (context, props, updateProp) {
+          return ListView(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: <Widget>[
+              DropdownPropUpdater(
+                props: props,
+                updateProp: updateProp,
+                propKey: 'bgColor',
+                options: _colorList,
+              )
+            ],
+          );
+        },
+        widgetBuilder: (_, props) {
+          return StatefulBuilder(builder: (ctx, stateSetter) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 10),
+                OdometerBadge(
+                  _getTextRuns(_initial, _current),
+                  bgColor: _optionToColor(props['bgColor']),
+                  baseColor: AppColor.deepWhite,
+                  textStyle: textStyle,
+                ),
+                const SizedBox(height: 10),
+                RaisedButton(
+                  child: const Text('update', style: const TextStyle(color: Colors.white)),
+                  onPressed: () {
+                    stateSetter(() {
+                      _initial = _current;
+                      _current = _initial == 0 ? 0.5 : _initial * 1.2;
+                    });
+                  },
+                )
+              ],
+            );
+          });
+        },
+      ),
+    );
+  }
+
+  List<UnstyledTextRun> _getTextRuns(double val, double newVal) {
+    final parts = '€${val.toStringAsFixed(2)}'.split('.');
+    if (parts.length == 2) {
+      parts.insert(1, '.');
+    }
+
+    final newParts = '€${newVal.toStringAsFixed(2)}'.split('.');
+    if (newParts.length == 2) {
+      newParts.insert(1, '.');
+    }
+    final runs = <UnstyledTextRun>[
+      UnstyledTextRun(newParts[0], parts[0]),
+    ];
+
+    if (parts.length > 1) {
+      runs.add(UnstyledTextRun(newParts[1], parts[1]));
+    }
+
+    if (parts.length > 2) {
+      runs.add(UnstyledTextRun(newParts[2], parts[2]));
+    }
+    return runs;
+  }
+}
