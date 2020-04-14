327816a3201aea5c37ff814e3566b13d61b15250
Tomi Alagbe <tomialagbe@yahoo.com>
2020-04-14 23:00:51 +0100

    increase animation duration for single digit

--- lib/widgets/odometer/single_digit.dart
+++ lib/widgets/odometer/single_digit.dart
@@ -68,7 +68,7 @@ class _SingleDigitState extends State<SingleDigit> with TickerProviderStateMixin
       numCycles = currentValue - finalValue;
     }
 
-    final durationMillis = numCycles > 0 ? 500 ~/ numCycles : 500;
+    final durationMillis = numCycles > 0 ? 900 ~/ numCycles : 500;
     controller = AnimationController(duration: Duration(milliseconds: durationMillis), vsync: this);
     final curvedAnimation = CurvedAnimation(parent: controller, curve: Curves.easeOutQuint);
     animation = Tween<double>(begin: currentValue.toDouble(), end: finalValue.toDouble()).animate(curvedAnimation)
