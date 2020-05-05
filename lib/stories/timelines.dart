0d581b4ed3faedaf68f2fed8af0b3a46a3208e31
Tomi Alagbe <tomialagbe@yahoo.com>
2020-05-05 19:24:04 +0100

    Implement timeline component

--- lib/stories/timelines.dart
+++ lib/stories/timelines.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_kit/widgets/progress_timeline.dart';
+import 'package:flutter_ui_kit/color.dart';
+import 'package:flutter_ui_kit/story_book/expandable_story.dart';
+import 'package:flutter_ui_kit/widgets/timeline/timeline.dart';
 
 class Timelines extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return const ProgressTimeline([]);
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          _timeline(),
+        ],
+      ),
+    );
+  }
+
+  Widget _timeline() {
+    return ExpandableStory(
+      title: 'Timeline',
+      child: Timeline(
+        activeLineColor: AppColor.green,
+        itemGap: 50,
+        children: <TimelineModel>[
+          TimelineModel(widget: _buildTimelineItem('Previous interest payday', 'Mar 1', true, true), isActive: true),
+          TimelineModel(
+              widget: _buildTimelineItem(
+                  'Deposit funds to earn 2.00% APY after it\'s transferd to our Service Providers ',
+                  'Transfer funds to Interest',
+                  false,
+                  true),
+              isActive: false),
+          TimelineModel(widget: _buildTimelineItem('Next interest payday', 'Apr 1', false, false), isActive: false),
+        ],
+        indicators: <Widget>[
+          Icon(Icons.check_circle, color: AppColor.green),
+          Icon(Icons.check_circle_outline, color: AppColor.semiGrey),
+          Icon(Icons.check_circle_outline, color: AppColor.semiGrey),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimelineItem(String title, String linkText, bool isActive, bool linkIsActive) {
+    final titleStyle = TextStyle(fontSize: 14, height: 20 / 14, color: isActive ? Colors.black : AppColor.semiGrey);
+    final linkStyle = titleStyle.copyWith(color: linkIsActive ? AppColor.green : AppColor.semiGrey);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title, style: titleStyle, textAlign: TextAlign.start),
+        Text(linkText, style: linkStyle, textAlign: TextAlign.start),
+      ],
+    );
   }
 }
---
+++ lib/widgets/timeline/timeline.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+
+class TimelineModel {
+  final Widget widget;
+  final bool isActive;
+
+  TimelineModel({@required this.widget, @required this.isActive});
+}
+
+class Timeline extends StatelessWidget {
+  const Timeline({
+    @required this.children,
+    this.indicators,
+    this.isLeftAligned = true,
+    this.itemGap = 12.0,
+    this.gutterSpacing = 4.0,
+    this.padding = const EdgeInsets.all(8),
+    this.controller,
+    this.lineColor = Colors.grey,
+    this.activeLineColor = Colors.grey,
+    this.physics,
+    this.shrinkWrap = true,
+    this.primary = false,
+    this.reverse = false,
+    this.indicatorSize = 30.0,
+    this.lineGap = 0.0,
+    this.indicatorColor = Colors.blue,
+    this.indicatorStyle = PaintingStyle.fill,
+    this.strokeCap = StrokeCap.butt,
+    this.strokeWidth = 3.0,
+    this.style = PaintingStyle.stroke,
+  })  : itemCount = children.length,
+        assert(itemGap >= 0),
+        assert(lineGap >= 0),
+        assert(indicators == null || children.length == indicators.length);
+
+  final List<TimelineModel> children;
+  final double itemGap;
+  final double gutterSpacing;
+  final List<Widget> indicators;
+  final bool isLeftAligned;
+  final EdgeInsets padding;
+  final ScrollController controller;
+  final int itemCount;
+  final ScrollPhysics physics;
+  final bool shrinkWrap;
+  final bool primary;
+  final bool reverse;
+
+  final Color lineColor;
+  final Color activeLineColor;
+  final double lineGap;
+  final double indicatorSize;
+  final Color indicatorColor;
+  final PaintingStyle indicatorStyle;
+  final StrokeCap strokeCap;
+  final double strokeWidth;
+  final PaintingStyle style;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: padding,
+      separatorBuilder: (_, __) => SizedBox(height: itemGap),
+      physics: physics,
+      shrinkWrap: shrinkWrap,
+      itemCount: itemCount,
+      controller: controller,
+      reverse: reverse,
+      primary: primary,
+      itemBuilder: (context, index) {
+        final child = children[index];
+        final prevChild = index == 0 ? null : children[index - 1];
+
+        Widget indicator;
+        if (indicators != null) {
+          indicator = indicators[index];
+        }
+
+        final isFirst = index == 0;
+        final isLast = index == itemCount - 1;
+
+        final timelineTile = <Widget>[
+          CustomPaint(
+            foregroundPainter: _TimelinePainter(
+              hideDefaultIndicator: indicator != null,
+              lineColor: lineColor,
+              activeLineColor: activeLineColor,
+              isActive: child.isActive,
+              isPrevActive: prevChild?.isActive ?? false,
+              indicatorColor: indicatorColor,
+              indicatorSize: indicatorSize,
+              indicatorStyle: indicatorStyle,
+              isFirst: isFirst,
+              isLast: isLast,
+              lineGap: lineGap,
+              strokeCap: strokeCap,
+              strokeWidth: strokeWidth,
+              style: style,
+              itemGap: itemGap,
+            ),
+            child: SizedBox(
+              height: double.infinity,
+              width: indicatorSize,
+              child: indicator,
+            ),
+          ),
+          SizedBox(width: gutterSpacing),
+          Expanded(child: child.widget),
+        ];
+
+        return IntrinsicHeight(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: isLeftAligned ? timelineTile : timelineTile.reversed.toList(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _TimelinePainter extends CustomPainter {
+  _TimelinePainter({
+    @required this.hideDefaultIndicator,
+    @required this.indicatorColor,
+    @required this.indicatorStyle,
+    @required this.indicatorSize,
+    @required this.lineGap,
+    @required this.strokeCap,
+    @required this.strokeWidth,
+    @required this.style,
+    @required this.lineColor,
+    @required this.activeLineColor,
+    @required this.isFirst,
+    @required this.isLast,
+    this.isActive = false,
+    this.isPrevActive = false,
+    @required this.itemGap,
+  })  : linePaint = Paint()
+    ..color = lineColor
+    ..strokeCap = strokeCap
+    ..strokeWidth = strokeWidth
+    ..style = style,
+        circlePaint = Paint()
+          ..color = indicatorColor
+          ..style = indicatorStyle;
+
+  final bool hideDefaultIndicator;
+  final Color indicatorColor;
+  final PaintingStyle indicatorStyle;
+  final double indicatorSize;
+  final double lineGap;
+  final StrokeCap strokeCap;
+  final double strokeWidth;
+  final PaintingStyle style;
+  final Color lineColor;
+  final Color activeLineColor;
+  final Paint linePaint;
+  final Paint circlePaint;
+  final bool isFirst;
+  final bool isLast;
+  final bool isActive;
+  final bool isPrevActive;
+  final double itemGap;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final indicatorRadius = indicatorSize / 2;
+    final halfItemGap = itemGap / 2;
+    final indicatorMargin = indicatorRadius + lineGap;
+
+    final top = size.topLeft(Offset(indicatorRadius, 0.0 - halfItemGap));
+    final centerTop = size.centerLeft(
+      Offset(indicatorRadius, -indicatorMargin),
+    );
+
+    final bottom = size.bottomLeft(Offset(indicatorRadius, 0.0 + halfItemGap));
+    final centerBottom = size.centerLeft(
+      Offset(indicatorRadius, indicatorMargin),
+    );
+
+    if (!isFirst) {
+      if (isPrevActive) linePaint.color = activeLineColor;
+      canvas.drawLine(top, centerTop, linePaint);
+      linePaint.color = lineColor;
+    }
+
+    if (!isLast) {
+      if (isActive) linePaint.color = activeLineColor;
+      canvas.drawLine(centerBottom, bottom, linePaint);
+      linePaint.color = lineColor;
+    }
+
+    if (!hideDefaultIndicator) {
+      final Offset offsetCenter = size.centerLeft(Offset(indicatorRadius, 0));
+
+      canvas.drawCircle(offsetCenter, indicatorRadius, circlePaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
