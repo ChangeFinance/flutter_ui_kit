fe2321918190f5b235866f49d13091bed35d6421
Tomi Alagbe <tomialagbe@yahoo.com>
2020-08-11 16:56:57 +0300

    Al update radio item (#193)
    * optimisation for odometer
    
    * update radio item to allow for disabling the radio item

--- lib/widgets/customradio/radio_model.dart
+++ lib/widgets/customradio/radio_model.dart
@@ -2,8 +2,16 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class RadioModel {
   bool isSelected;
+  bool isEnabled;
   final String text;
   SvgPicture svgPicture;
   final String textShortform;
-  RadioModel(this.text, {this.svgPicture, this.isSelected, this.textShortform});
+
+  RadioModel(
+    this.text, {
+    this.svgPicture,
+    this.isSelected,
+    this.textShortform,
+    this.isEnabled = true,
+  });
 }
