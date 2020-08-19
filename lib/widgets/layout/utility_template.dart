8b52367238bf77cc5deb016bc0bf60d6ea85f41b
valeriya makusheva <vmakusheva@rightandabove.com>
2020-08-19 12:36:43 +0300

    added utility generic template

--- lib/widgets/layout/utility_template.dart
+++ lib/widgets/layout/utility_template.dart
@@ -2,71 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_ui_kit/color.dart';
 import 'package:flutter_ui_kit/text.dart';
-import 'package:flutter_ui_kit/widgets/layout/page_template.dart';
-import 'package:flutter_ui_kit/widgets/text/heading.dart';
-import 'package:flutter_ui_kit/widgets/text/heading_type.dart';
+import 'package:flutter_ui_kit/widgets/layout/utility_generic_template.dart';
 
-import 'app_bar.dart';
 
-class UtilityTemplate extends StatelessWidget {
-  final SvgPicture picture;
-  final String heading;
-  final String body;
-  final Widget action;
-  final bool restrictWidth;
-  final bool implyLeading;
+class UtilityTemplate extends UtilityGenericTemplate {
 
-  const UtilityTemplate(this.heading, this.body, this.picture, this.action,
-      {this.restrictWidth = true, this.implyLeading = false})
-      : super();
-
-  @override
-  Widget build(BuildContext context) {
-    return PageTemplate(
-      appBar: MainAppBar(implyLeading: implyLeading),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 34.0),
-                  child: picture,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                constraints: restrictWidth ? BoxConstraints(maxWidth: picture.width): const BoxConstraints(),
-                child: Column(
-                  children: <Widget>[
-                    Heading(
-                      heading,
-                      headingType: HeadingType.HEADING_1,
-                      textAlign: TextAlign.center,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Text(body,
-                          textAlign: TextAlign.center,
-                          style:
-                              AppText.body1.copyWith(color: AppColor.semiGrey)),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: action,
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  UtilityTemplate(String heading, String bodyAsText, SvgPicture picture, Widget action,
+      {bool restrictWidth = true, bool implyLeading = false})
+      : super(
+              heading,
+              Text(bodyAsText,
+                  textAlign: TextAlign.center,
+                  style: AppText.body1.copyWith(color: AppColor.semiGrey)),
+              picture,
+              action,
+              shouldRestrictWidth: restrictWidth,
+              shouldImplyLeading: implyLeading);
 }
