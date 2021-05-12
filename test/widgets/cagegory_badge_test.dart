02cac6da23e82ad2f56d5e30a7ba501471544a35
Vadim Gulyakin <vgulyakin@rightandabove.com>
2021-05-12 17:20:02 +0300

    [EN-1896] Create tab categories component (#212)

---
+++ test/widgets/cagegory_badge_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_ui_kit/color.dart';
+import 'package:flutter_ui_kit/widgets/badge/category_badge.dart';
+
+import '../wrap_in_material_app.dart';
+
+void main() {
+  group('Category Badge', () {
+    testWidgets('renders default category badge widget', (WidgetTester tester) async {
+      await tester.pumpWidget(wrapInMaterialApp(
+          CategoryBadge(
+              text: 'Badge Content',
+              tagText: '21',
+              name: 'BADGE',
+              selected: false
+          )
+      ));
+
+      expect(find.byType(CategoryBadge), findsOneWidget);
+      expect(find.text('Badge Content'), findsOneWidget);
+      expect(find.text('21'), findsOneWidget);
+      final widget = tester.widget(find.byKey(const Key('BADGE_container')));
+      final Container containerWidget = widget;
+      final BoxDecoration decoration = containerWidget.decoration;
+      expect(decoration.color, AppColor.deepWhite);
+      expect(decoration.borderRadius, const BorderRadius.all(Radius.circular(18)));
+      expect(decoration.border, const Border.fromBorderSide(BorderSide(color: AppColor.semiGrey)));
+    });
+
+    testWidgets('renders selected category badge widget', (WidgetTester tester) async {
+      await tester.pumpWidget(wrapInMaterialApp(
+          CategoryBadge(
+              text: 'Badge Content',
+              tagText: '21',
+              name: 'BADGE',
+              selected: true
+          )
+      ));
+
+      expect(find.byType(CategoryBadge), findsOneWidget);
+      expect(find.text('Badge Content'), findsOneWidget);
+      expect(find.text('21'), findsOneWidget);
+      final widget = tester.widget(find.byKey(const Key('BADGE_container')));
+      final Container containerWidget = widget;
+      final BoxDecoration decoration = containerWidget.decoration;
+      expect(decoration.color, AppColor.green);
+      expect(decoration.borderRadius, const BorderRadius.all(Radius.circular(18)));
+      expect(decoration.border, const Border.fromBorderSide(BorderSide(color: AppColor.deepWhite)));
+    });
+  });
+}
